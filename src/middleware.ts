6342dfe222648037ec1d37d8ffb8a6884a592466
Ahmed Mansour <133324agh@gmail.com>
2024-06-21 13:07:36 +0300

    fix  clerkMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,18 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/forum(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req))
-    auth().redirectToSignIn({
-      returnBackUrl: "/",
-    });
-  
-}, {
-  signInUrl: "/auth/Sign-in",
-  redirectUrl: "/",
-  signUpUrl: "/auth/Sigin-up",
-});
+export default clerkMiddleware(
+  (auth, req) => {
+    if (isProtectedRoute(req))
+      auth().redirectToSignIn({
+        returnBackUrl: "/",
+      });
+  },
+  {
+    signInUrl: "/auth/Sign-in",
+    signUpUrl: "/auth/Sigin-up",
+  }
+);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
