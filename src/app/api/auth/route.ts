eb5c4a1c92d8b058f54928dd2ff13ccb6bd1effc
Ahmed Mansour <133324agh@gmail.com>
2024-06-21 13:10:49 +0300

    c

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -2,17 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { userId } = getAuth(req);
 
-  if (!userId) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  // retrieve data from your database
-
-  res.status(200).json({});
-}
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { userId } = getAuth(req);
   if (!userId) {
